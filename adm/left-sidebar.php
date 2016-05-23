c3af2b1c066e68da33a80b58f1fac5e8a2766201
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2016-05-22 23:38:58 -0300

    Acréscimos de segurança e layout
    Acrescentado um icone fav. Correção no layout de consulta. Agora existe
    um validador de email, para que duas contas não tenham mesmo email.
    Senhas a partir deste momento deverão possuir no mínimo 8 caracteres

--- adm/left-sidebar.php
+++ adm/left-sidebar.php
@@ -29,7 +29,7 @@
 								<div class="panel-heading">
 									<h4 class="panel-title">
 										<a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" class="collapsed"><span class="glyphicon glyphicon-th">
-										</span>Modules</a>
+										</span> Games</a>
 									</h4>
 								</div>
 								<div id="collapseTwo" class="panel-collapse collapse" style="height: 0px;">
@@ -37,57 +37,12 @@
 										<table class="table">
 											<tbody><tr>
 												<td>
-													<a href="http://www.jquery2dotnet.com">Orders</a> <span class="label label-success">$ 320</span>
+													<span class="glyphicon glyphicon-pencil text-primary"></span><a href="cadgame.php"> Cadastrar </a>
 												</td>
 											</tr>
 											<tr>
 												<td>
-													<a href="http://www.jquery2dotnet.com">Invoices</a>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<a href="http://www.jquery2dotnet.com">Shipments</a>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<a href="http://www.jquery2dotnet.com">Tex</a>
-												</td>
-											</tr>
-										</tbody></table>
-									</div>
-								</div>
-							</div>
-							<div class="panel panel-default">
-								<div class="panel-heading">
-									<h4 class="panel-title">
-										<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" class="collapsed"><span class="glyphicon glyphicon-user">
-										</span>Account</a>
-									</h4>
-								</div>
-								<div id="collapseThree" class="panel-collapse collapse" style="height: 0px;">
-									<div class="panel-body">
-										<table class="table">
-											<tbody><tr>
-												<td>
-													<a href="http://www.jquery2dotnet.com">Change Password</a>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<a href="http://www.jquery2dotnet.com">Notifications</a> <span class="label label-info">5</span>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<a href="http://www.jquery2dotnet.com">Import/Export</a>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<span class="glyphicon glyphicon-trash text-danger"></span><a href="http://www.jquery2dotnet.com" class="text-danger">
-														Delete Account</a>
+													<span class="glyphicon glyphicon-file text-info"></span><a href="consulgame.php"> Consultar </a>
 												</td>
 											</tr>
 										</tbody></table>
@@ -98,7 +53,7 @@
 								<div class="panel-heading">
 									<h4 class="panel-title">
 										<a data-toggle="collapse" data-parent="#accordion" href="#collapseFour" class=""><span class="glyphicon glyphicon-file">
-										</span>Reports</a>
+										</span> Relarórios</a>
 									</h4>
 								</div>
 								<div id="collapseFour" class="panel-collapse in" style="height: auto;">
@@ -106,22 +61,7 @@
 										<table class="table">
 											<tbody><tr>
 												<td>
-													<span class="glyphicon glyphicon-usd"></span><a href="http://www.jquery2dotnet.com">Sales</a>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<span class="glyphicon glyphicon-user"></span><a href="http://www.jquery2dotnet.com">Customers</a>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<span class="glyphicon glyphicon-tasks"></span><a href="http://www.jquery2dotnet.com">Products</a>
-												</td>
-											</tr>
-											<tr>
-												<td>
-													<span class="glyphicon glyphicon-shopping-cart"></span><a href="http://www.jquery2dotnet.com">Shopping Cart</a>
+													<span class="glyphicon glyphicon glyphicon-save-file"></span><a href="">Relatório de Jogos</a>
 												</td>
 											</tr>
 										</tbody></table>
